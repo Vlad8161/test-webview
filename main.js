c5c28f36d42ef204be5ed2447114e77e785a763f
Vlad <vlad81611@yandex.ru>
2021-01-20 22:26:12 +0300

    Port 80

--- main.js
+++ main.js
@@ -32,7 +32,7 @@ app.get('/link', (req, res) => {
   res.render('index', {message: `POST: ${req.headers} ${req.params} ${req.body}`})
 })
 
-app.listen(3000, () => {
+app.listen(80, () => {
   console.log('Server is running on port 3000')
 })
 
